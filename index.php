66785b3f60cddf2761ac7ac5b479b9883553244c
Awitadefresa <146397822+danimercado20@users.noreply.github.com>
2026-09-17 15:30:35 -0600

    Creacion de Html con style

---
+++ index.php
@@ -0,0 +1,51 @@
+<!--  <?php
+    echo "Hello, World!";
+?>-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            color: #484fda;
+        }
+        h2 {
+            color: #ffeb3b;
+        }
+        h3 {
+            color: #9c27b0;
+        }
+        h4 {
+            color: #e20909;
+        }
+        h5 {
+            color: #9599ee;
+        }
+        h6 {
+            color: #86eda5;
+        }
+        </style>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <h2>Hello, World!</h2>
+    <h3>Hello, World!</h3>
+    <h4>Hello, World!</h4>
+    <h5>Hello, World!</h5>
+    <h6>Hello, World!</h6>
+    
+
+
+
+
+    
+</body>
+</html>

dedb936d11dea5c1ad8cb211764b27179d8e48d5
Awitadefresa <146397822+danimercado20@users.noreply.github.com>
2026-09-18 16:59:35 -0600

    Probando padding , margin y border

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
         body {
             
             font-family: Arial, sans-serif;
-            text-align: center;
-            padding: 50px;
+          
+          
         }
         h1 {
             color: #484fda;
@@ -32,6 +32,30 @@
         h6 {
             color: #77f89d;
         }
+        .section
+        {
+            padding : 20px 40px;
+            background-color: #aadfaa;
+            margin:20px;
+            border: 5px none #ffa7ec;
+
+            
+        }
+        .section2
+        {
+            padding-right: 40px;
+            padding-left: 40px;
+            padding-top: 20px;
+            padding-bottom: 20px;
+            background-color: #f9d6d6;
+            margin-top: 20px;
+            margin-bottom: 40px;
+            margin-right: 30px;
+            margin-left: 50px;
+            border: 14px hidden #4ebda3;
+           
+            
+        }
         </style>
 </head>
 <body>
@@ -41,6 +65,23 @@
     <h4>Hello, World!</h4>
     <h5>Hello, World!</h5>
     <h6>Hello, World!</h6>
+
+    <div class="section">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.</p>
+
+    </div>
+    <div class="section2">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.</p>
+
+    </div>
     
 
 
